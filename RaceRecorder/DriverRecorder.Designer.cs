f21248108bfc78ce5471c107673b521597c43d18
dnaka <daisuke.nakagawa@jp.ricoh.com>
2022-01-06 06:05:58 +0900

    Ist Commit

---
+++ RaceRecorder/DriverRecorder.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace RaceRecorder
+{
+    partial class RaceRecorder
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RaceRecorder));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.RaceNameTextBox = new System.Windows.Forms.TextBox();
+            this.RaceNamelabel = new System.Windows.Forms.Label();
+            this.DriverListView = new System.Windows.Forms.DataGridView();
+            this.driverEntryLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.DriverNumberTextBox = new System.Windows.Forms.TextBox();
+            this.registButton = new System.Windows.Forms.Button();
+            this.lapTimer = new System.Windows.Forms.Timer(this.components);
+            this.resultViewButton = new System.Windows.Forms.Button();
+            this.validateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StateLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DriverListView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // RaceNameTextBox
+            // 
+            resources.ApplyResources(this.RaceNameTextBox, "RaceNameTextBox");
+            this.RaceNameTextBox.Name = "RaceNameTextBox";
+            // 
+            // RaceNamelabel
+            // 
+            resources.ApplyResources(this.RaceNamelabel, "RaceNamelabel");
+            this.RaceNamelabel.Name = "RaceNamelabel";
+            // 
+            // DriverListView
+            // 
+            this.DriverListView.AllowUserToResizeRows = false;
+            resources.ApplyResources(this.DriverListView, "DriverListView");
+            this.DriverListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DriverListView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.DriverListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.DriverListView.DefaultCellStyle = dataGridViewCellStyle6;
+            this.DriverListView.MultiSelect = false;
+            this.DriverListView.Name = "DriverListView";
+            this.DriverListView.RowTemplate.Height = 21;
+            // 
+            // driverEntryLabel
+            // 
+            resources.ApplyResources(this.driverEntryLabel, "driverEntryLabel");
+            this.driverEntryLabel.Name = "driverEntryLabel";
+            // 
+            // startButton
+            // 
+            resources.ApplyResources(this.startButton, "startButton");
+            this.startButton.Name = "startButton";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // TimeLabel
+            // 
+            resources.ApplyResources(this.TimeLabel, "TimeLabel");
+            this.TimeLabel.Name = "TimeLabel";
+            // 
+            // DriverNumberTextBox
+            // 
+            resources.ApplyResources(this.DriverNumberTextBox, "DriverNumberTextBox");
+            this.DriverNumberTextBox.Name = "DriverNumberTextBox";
+            this.DriverNumberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.DriverNumberTextBox_KeyPress);
+            // 
+            // registButton
+            // 
+            resources.ApplyResources(this.registButton, "registButton");
+            this.registButton.Name = "registButton";
+            this.registButton.UseVisualStyleBackColor = true;
+            this.registButton.Click += new System.EventHandler(this.registButton_Click);
+            // 
+            // lapTimer
+            // 
+            this.lapTimer.Interval = 500;
+            this.lapTimer.Tick += new System.EventHandler(this.lapTimer_Tick);
+            // 
+            // resultViewButton
+            // 
+            resources.ApplyResources(this.resultViewButton, "resultViewButton");
+            this.resultViewButton.Name = "resultViewButton";
+            this.resultViewButton.UseVisualStyleBackColor = true;
+            this.resultViewButton.Click += new System.EventHandler(this.resultViewButton_Click);
+            // 
+            // validateButton
+            // 
+            resources.ApplyResources(this.validateButton, "validateButton");
+            this.validateButton.Name = "validateButton";
+            this.validateButton.UseVisualStyleBackColor = true;
+            this.validateButton.Click += new System.EventHandler(this.validateButton_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // StateLabel
+            // 
+            resources.ApplyResources(this.StateLabel, "StateLabel");
+            this.StateLabel.Name = "StateLabel";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.StateLabel);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // RaceRecorder
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.validateButton);
+            this.Controls.Add(this.resultViewButton);
+            this.Controls.Add(this.registButton);
+            this.Controls.Add(this.DriverNumberTextBox);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.driverEntryLabel);
+            this.Controls.Add(this.DriverListView);
+            this.Controls.Add(this.RaceNamelabel);
+            this.Controls.Add(this.RaceNameTextBox);
+            this.Name = "RaceRecorder";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DriverListView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox RaceNameTextBox;
+        private System.Windows.Forms.Label RaceNamelabel;
+        private System.Windows.Forms.DataGridView DriverListView;
+        private System.Windows.Forms.Label driverEntryLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.TextBox DriverNumberTextBox;
+        private System.Windows.Forms.Button registButton;
+        private System.Windows.Forms.Timer lapTimer;
+        private System.Windows.Forms.Button resultViewButton;
+        private System.Windows.Forms.Button validateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label StateLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ RaceRecorder/DriverRecorder.cs
@@ -0,0 +1,468 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RaceRecorder
+{
+    enum State
+    {
+        RESISTING,  // 初期状態で登録中またはレース終了後
+        WAIT_START, // 登録完了してスタート待
+        RACING      // レース中
+    }
+
+    public partial class RaceRecorder : Form
+    {
+        private DataTable driverTable;
+        private DateTime startTime;
+        private State state = State.RESISTING;
+        private Dictionary<string, List<string>> records;
+        private string driverCache;
+        private string recordsCache;
+
+        private static string NO = "ゼッケン";
+        private static string NAME = "氏名";
+        private static string LAP = "LAP数";
+        private static string LAPTIME = "最終LapTime";
+
+        public RaceRecorder()
+        {
+            InitializeComponent();
+            this.records = new Dictionary<string, List<string>>();
+
+            this.driverCache = Path.GetTempPath() + "DriverRecorderCache.csv";
+            this.recordsCache = Path.GetTempPath() + "DriverRecorderRecordsCache.csv";
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            setupDriverList();
+            setControlFromState(State.RESISTING);
+        }
+
+        private void setupDriverList()
+        {
+            this.driverTable = new DataTable("driverList");
+            this.driverTable.Columns.Add(RaceRecorder.NO, Type.GetType("System.String"));
+            this.driverTable.Columns.Add(RaceRecorder.NAME, Type.GetType("System.String"));
+            this.driverTable.Columns.Add(RaceRecorder.LAP, Type.GetType("System.UInt32"));
+            this.driverTable.Columns.Add(RaceRecorder.LAPTIME, Type.GetType("System.String"));
+            this.driverTable.Columns[2].ReadOnly = true;
+            this.driverTable.Columns[3].ReadOnly = true;
+            loadCache();
+            DriverListView.DataSource = this.driverTable;
+        }
+
+        private void loadCache()
+        {
+            try
+            {
+               ;
+
+                if (File.Exists(this.driverCache) && File.Exists(this.recordsCache))
+                {
+                    // driverTableの生成
+                    string[] lines = File.ReadAllLines(this.driverCache);
+                    this.RaceNameTextBox.Text = lines[0].Split(',')[0];
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        string[] cols = lines[i].Split(',');
+                        DataRow r = this.driverTable.NewRow();
+                        r.SetField(0, cols[0]);
+                        r.SetField(1, cols[1]);
+                        r.SetField(2, Int32.Parse(cols[2]));
+                        r.SetField(3, cols[3]);
+                        this.driverTable.Rows.Add(r);
+                    }
+
+                    // records側の生成
+                    this.records = new Dictionary<string, List<string>>();
+                    string[] lines2 = File.ReadAllLines(this.recordsCache);
+
+                    for (int i = 0; i < lines2.Length; i++)
+                    {
+                        string[] cols = lines2[i].Split(',');
+
+                        List<string> record = new List<string>(); 
+                        for (int j = 1; j < cols.Length; j++)
+                        {
+                            record.Add(cols[j]);
+                        }
+                        this.records.Add(cols[0].ToString(), record);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                this.clearCache();
+            }
+        }
+
+        // エラー時などやクリアボタン押下時にデータを初期化する
+        private void clearCache()
+        {
+            this.driverTable.Clear();
+            this.records.Clear();
+            this.RaceNameTextBox.Text = "";
+
+            try
+            {
+                File.Delete(this.driverCache);
+                File.Delete(this.recordsCache);
+            } 
+            catch (Exception e) 
+                {
+                Console.WriteLine("Cache Remove failed, " + e.Message);
+            }
+
+            setControlFromState(State.RESISTING);
+
+        }
+
+        private void setControlFromState(State toState)
+        {
+            switch (toState)
+            {
+                case State.RESISTING:
+                    // 起動後か、RACING中にスタートをおしてタイマーを止めたらここ
+                    DriverListView.Enabled = true;
+                    validateButton.Enabled = true;
+                    startButton.Enabled = false;
+                    resultViewButton.Enabled = true;
+                    StateLabel.Text = "ドライバー登録中/レース終了";
+                    registButton.Enabled = false;
+                    state = State.RESISTING;
+                    break;
+
+                case State.WAIT_START:
+                    // 確定ボタンを押したらここ
+                    DriverListView.Enabled = true;
+                    validateButton.Enabled = true;
+                    startButton.Enabled = true;
+                    resultViewButton.Enabled = true;
+                    StateLabel.Text = "スタート可能";
+                    startButton.Text = "レーススタート";
+                    registButton.Enabled = false;
+                    resetLapTime();
+                    state = State.WAIT_START;
+                    break;
+            
+                case State.RACING:
+                    // スタートボタンを推してタイマーを開始したらここ
+                    DriverListView.Enabled = false;
+                    validateButton.Enabled = false;
+                    startButton.Enabled = true;
+                    resultViewButton.Enabled = false;
+                    registButton.Enabled = true;
+                    StateLabel.Text = "レース中";
+                    startButton.Text = "レース停止";
+                    DriverNumberTextBox.Focus();
+                    state = State.RACING;
+                    break;
+            }
+        }
+
+        //スタート時にラップタイムの入力をリセットする
+        private void resetLapTime()
+        {
+            TimeLabel.Text = "00:00:00";
+            this.driverTable.Columns[2].ReadOnly = false;
+            this.driverTable.Columns[3].ReadOnly = false;
+            for (int i = 0; i < this.driverTable.Rows.Count; i++)
+            {
+                if (this.driverTable.Rows[i][0] != null &&
+                    driverTable.Rows[i][1] != null)
+                {
+                    this.driverTable.Rows[i][0] = this.driverTable.Rows[i][0].ToString().Trim();
+                    this.driverTable.Rows[i][1] = this.driverTable.Rows[i][1].ToString().Trim();
+                    this.driverTable.Rows[i][2] = 0;
+                    this.driverTable.Rows[i][3] = "00:00:00";
+                }
+            }
+            this.driverTable.Columns[2].ReadOnly = true;
+            this.driverTable.Columns[3].ReadOnly = true;
+        }
+
+        private bool validateDriverInfo()
+        {
+            int i = 0;
+            DataRowCollection c = this.driverTable.Rows;
+            while (i < c.Count)
+            {
+
+                object noCell = this.driverTable.Rows[i][0];
+                object nameCell = this.driverTable.Rows[i][1];
+
+                bool isNoNull = (noCell == null || string.IsNullOrEmpty(noCell.ToString()));
+                bool isNameNull = (nameCell == null || string.IsNullOrEmpty(nameCell.ToString()));
+
+                // 両方ない行は削除
+                if ((!isNoNull && isNameNull) || (isNoNull && !isNameNull))
+                {
+                    // アイコンも指定
+                    MessageBox.Show("ゼッケンか名前が未入力の選手がいます", "ドライバー登録エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+                i++;     
+            }
+
+            for (i = 0; i < this.driverTable.Rows.Count; i++)
+            {
+                for (int j = i + 1; j < this.driverTable.Rows.Count; j++)
+                {
+                    if (this.driverTable.Rows[i][0].ToString().Equals(this.driverTable.Rows[j][0].ToString()))
+                    {
+                        // アイコンも指定
+                        MessageBox.Show("同じゼッケンの選手がいます", "ドライバー登録エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        // ドライバー登録後の登録完了ボタン押下
+        private void validateButton_Click(object sender, EventArgs e)
+        {
+            if(validateDriverInfo())
+            {
+                setControlFromState(State.WAIT_START);
+
+                // recordsを初期化
+                this.records = new Dictionary<string, List<string>>();
+                foreach (DataRow r in this.driverTable.Rows)
+                {
+                    this.records.Add(r[0].ToString(), new List<string>());
+                }
+                // キャッシュ更新
+                saveCache();
+            }
+            else
+            {
+                setControlFromState(State.RESISTING);
+            }            
+        }
+
+        // スタートボタン押下イベント
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (state == State.WAIT_START) {
+                setControlFromState(State.RACING);
+                startTime = DateTime.Now;
+                lapTimer.Enabled = true;
+            }
+            else
+            {
+                lapTimer.Enabled = false;
+                saveCache();
+                setControlFromState(State.RESISTING);
+            }
+        }
+
+
+        // ラップ通過記録ボタン押下イベント
+        private void registButton_Click(object sender, EventArgs e)
+        {
+            string no = DriverNumberTextBox.Text;
+            regist(no);
+            DriverNumberTextBox.Text = "";
+            DriverNumberTextBox.Focus();
+        }
+
+        // 記録の登録処理
+        private void regist(string no)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(no))
+                {
+                    return;
+                }
+
+                for (int i = 0; i < this.driverTable.Rows.Count; i++)
+                {
+
+                    if (this.driverTable.Rows[i][0].ToString() == no)
+                    {
+                        this.driverTable.Columns[2].ReadOnly = false;
+                        this.driverTable.Columns[3].ReadOnly = false;
+                        this.driverTable.Rows[i][2] = UInt32.Parse(DriverListView.Rows[i].Cells[2].Value.ToString()) + 1;
+                        this.driverTable.Rows[i][3] = getTimeAfterStart();
+                        this.driverTable.Columns[2].ReadOnly = true;
+                        this.driverTable.Columns[3].ReadOnly = true;
+
+                        this.records[no].Add(this.driverTable.Rows[i][3].ToString());
+
+                        break;
+                    }
+                }
+
+                this.driverTable.DefaultView.Sort = RaceRecorder.LAP + " DESC, " + RaceRecorder.LAPTIME + " ASC";
+                this.driverTable = this.driverTable.DefaultView.ToTable(true);
+                DriverListView.DataSource = this.driverTable;
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.Message);
+                return;
+            }
+        }
+
+        // スタート後のタイマー表示更新
+        private void lapTimer_Tick(object sender, EventArgs e)
+        {
+            TimeLabel.Text = getTimeAfterStart();
+        }
+
+        // スタート後の経過時間テキストを取得する
+        private string getTimeAfterStart()
+        {
+            if (state == State.RACING)
+            {
+                TimeSpan diff = DateTime.Now - startTime;
+                return diff.Hours.ToString("D2") + ":" + diff.Minutes.ToString("D2") + ":" + diff.Seconds.ToString("D2");
+            }
+            return "00:00:00";
+        }
+
+        // ゼッケン番号テキストボックスでEnterを推したらラップタイムの登録にかかる
+        private void DriverNumberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar ==(char)Keys.Enter)
+            {
+                regist(DriverNumberTextBox.Text);
+                DriverNumberTextBox.Text = "";
+            }
+        }
+
+        // 結果表示
+        private void resultViewButton_Click(object sender, EventArgs e)
+        {
+            string tmpFile =  makeResultHtml();
+
+            string url = "file://" + tmpFile.Replace("\\", "/");
+            System.Diagnostics.Process.Start(url);
+        }
+
+        private void saveCache()
+        {
+            string tmpFile = Path.GetTempPath() + "DriverRecorderCache.csv";
+            StringBuilder sb = new StringBuilder(this.RaceNameTextBox.Text + ",,,\r\n");
+            foreach (DataRow r in this.driverTable.Rows)
+            {
+                sb.Append(r[0] +"," + r[1] +"," + r[2] + "," + r[3] + "\r\n");
+            }
+            File.WriteAllText(tmpFile, sb.ToString());
+
+
+            // 個々のレコードの保存
+            string tmpFile2 = Path.GetTempPath() + "DriverRecorderRecordsCache.csv";
+            StringBuilder recordBuffer = new StringBuilder();
+            foreach (string key in this.records.Keys)
+            {
+                StringBuilder line = new StringBuilder(key + ",");
+                foreach (string record in this.records[key])
+                {
+                    line.Append(record + ",");
+                }
+
+                // 末尾についている,を削除
+                recordBuffer.Append(line.ToString().TrimEnd(',') + "\r\n");
+            }
+            File.WriteAllText(tmpFile2, recordBuffer.ToString());
+        }
+
+        // HTMLファイルを生成してtmpファイルを作る
+        private string makeResultHtml()
+        {
+            string tmpPath = Path.GetTempPath() + "result.html";            
+            int rank = 1;
+
+            StringBuilder sb = new StringBuilder();
+            foreach (DataRow r in this.driverTable.Rows)
+            {
+                string recordPath = Path.GetTempPath() + r[0] + ".html";
+                sb.Append(
+                    "<tr><td align=\"center\">" + rank.ToString() +
+                    "</td><td align=\"center\">" + r[0].ToString() +
+                    "</td><td align=\"center\"><a href=\"" + recordPath + "\">" + r[1].ToString() +
+                    "</a></td><td align=\"center\">" + r[2].ToString() +
+                    "</td><td align=\"center\">" + r[3].ToString() + "</td></tr>\r\n"
+                );
+
+                makeRecordhtml(r[0].ToString(), r[1].ToString(), recordPath);
+
+                rank++;
+            }
+
+            string table = 
+                "<table border=\"1\">\r\n" + 
+                "<tr><th>順位</th><th>" + 
+                RaceRecorder.NO +
+                "</th><th>" + 
+                RaceRecorder.NAME + 
+                "</th><th>" + 
+                RaceRecorder.LAP + 
+                "</th><th>" + 
+                RaceRecorder.LAPTIME + 
+                "</th></tr>\r\n" + 
+                sb.ToString() + 
+                "</table>\r\n";
+
+            string date = "<h3>" + DateTime.Now.ToLocalTime().ToShortDateString() + "</h3>\r\n";
+            string title = "<h1>" + this.RaceNameTextBox.Text + "</h1>\r\n";
+
+            string header = "<header><title>" + this.RaceNameTextBox.Text +"</title></header>";
+            string html = header + "<body>" + date + title + table + "</body>";
+            File.WriteAllText(tmpPath, html);
+
+            return tmpPath;
+        }
+
+        private void makeRecordhtml(string no, string name, string path)
+        {
+            int lap = 1;
+            List<string> record = this.records[no];
+            StringBuilder sb = new StringBuilder();
+            foreach (string time in record)
+            {
+                sb.Append(
+                  "<tr><td align=\"center\">" + lap.ToString() + "</a></td><td>" + time + "</td></tr>\r\n"
+                );
+                lap++;
+            }
+
+            string table =
+             "<table border=\"1\">\r\n" +
+             "<th>" +
+             RaceRecorder.LAP +
+             "</th><th>" +
+             RaceRecorder.LAPTIME +
+             "</th></tr>\r\n" +
+             sb.ToString() +
+             "</table>\r\n";
+
+            string date = "<h3>" + DateTime.Now.ToLocalTime().ToShortDateString() + "</h3>\r\n";
+            string title = "<h1>" + this.RaceNameTextBox.Text + "</h1>\r\n";
+            string rider = "<h2>" + name +"</h2>\r\n";
+
+            string header = "<header><title>" + this.RaceNameTextBox.Text + "</title></header>";
+            string html = header + "<body>" + date + title + rider + table + "</body>";
+            File.WriteAllText(path, html);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.clearCache();
+        }
+    }
+}
